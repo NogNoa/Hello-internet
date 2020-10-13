2ca1f2e8d228514111fbe0009abe63595e430eb9
NogNoa <53447424+NogNoa@users.noreply.github.com>
2020-10-13 18:23:09 +0300

    Add files via upload

--- GameFAQs.py
+++ GameFAQs.py
@@ -3,14 +3,34 @@ import TableRead as HI
 
 def ExtractDate(url):
     soup = HI.soupinit(url)
-    content = soup.find("div", attrs={"class": "body pod_gameinfo_left"}).find("ul")
+    content = soup.find("div", attrs={"class": "body pod_gameinfo_left"})
+    if not content:
+        content = soup.find("div", attrs={"class": "pod pod_gameinfo"})
+    content = content.find("ul")
     li = content.find_all("li")
-    date = li[4].find("a", href=True)
+
+    for pl in li:
+        b = HI.undiv(pl.find('b'))
+        if b[:3] == "Rel":  # "Release:"
+            date = pl.find("a", href=True)
+            continue
+    try:
+        date
+    except NameError:
+        print("unknown date:", url)
+        return "Unknown"
+
+    date = HI.undiv(date)
+    date = date.split(' ')
+    if len(date) > 2:
+        date = date[1] + date[0] + date[2]
+    date = "".join(date).replace(',', '')
     return date
 
 
 def ExtractTable(page_num):
     page = genpage + str(page_num)
+    print('\npage:', page)
     soup = HI.soupinit(page)
     table = soup.find().find("table", attrs={'class': 'results'}).find("tbody")
     for tr in table.find_all('tr'):
@@ -30,12 +50,10 @@ def ExtractTable(page_num):
 if __name__ == '__main__':
     home = "https://gamefaqs.gamespot.com"
     report = open(' GameFAQs.csv', 'w+')
-    platform = ''
-    genpage = home + "/games/rankings&min_votes=2?platform=" + platform + '?page='
+    genpage = home + "/games/rankings?list_type=rate&min_votes=2" + '?page='
     Rank = 5
     page_num = 1
-    report.write('Name,System,rating,date')
+    report.write('Name,System,rating,date,')
     while Rank >= 3.7:
-        print('\npage:', page_num)
         ExtractTable(page_num)
-        page_num += 1
+        HI.driver()
