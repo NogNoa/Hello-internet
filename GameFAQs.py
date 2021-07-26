c648481d09a223db6833c95442f1effa5b3aab54
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-07-26 13:57:49 +0300

    add extract_genres
    check existence of date instead of boolean
    change camel case to _ case

--- GameFAQs.py
+++ GameFAQs.py
@@ -1,23 +1,22 @@
 import TableRead as HI
 
 
-def ExtractDate(html):
+def extract_date(html):
     soup = HI.soupinit(html=html)
-    content = soup.find("div", attrs={"class": "body pod_gameinfo_left"})
+    content = soup.body.find("div", attrs={"class": "body pod_gameinfo_left"})
     if not content:
-        content = soup.find("div", attrs={"class": "pod pod_gameinfo"})
+        content = soup.body.find("div", attrs={"class": "pod pod_gameinfo"})
     if not content:
         print("no idea where right table is", html)
     content = content.ul
     lii = content.find_all("li")
-    date = ''
-    for pl in lii:
-        b = HI.undiv(pl.find('b'))
+    for li in lii:
+        b = HI.undiv(li.find('b'))
         if b[:3] == "Rel":  # "Release:"
-            date = pl.find("a", href=True)
+            date = li.find("a", href=True)
             break
 
-    if not date:
+    if "date" not in locals():
         errors.write("Unknown Date:" + html)
         return "Unknown"
 
@@ -31,15 +30,15 @@ def ExtractDate(html):
         if not date[0] in {'TBA', 'Canceled'}:  # we actually only looking for a year
             date = ['december', date[0]]
     else:
-        if date[0][0] == 'Q':  # the first charecter of the first word is Q, a quarter rather than day.
-            quart = int(date[0][1])  # the second charecter is the number of the quarter
+        if date[0][0] == 'Q':  # the first character of the first word is Q, a quarter rather than day.
+            quart = int(date[0][1])  # the second character is the number of the quarter
             month = 3 * quart
             date = [str(month), date[1]]
     date = "/".join(date).replace(',', '')
     return date
 
 
-def ExtractWankers(html, name):
+def extract_wankers(html, name):
     # wankers in actually rankers, it's a joke gimeabreak
     soup = HI.soupinit(html=html)
     rate_text = soup.find("div", attrs={"id": "gs_rate_avg_hint", "class": "gamespace_rate_hint"})
@@ -54,7 +53,31 @@ def ExtractWankers(html, name):
     return wankers
 
 
-def ExtractTable(page_num, cutoff_wankers=0):
+def extract_genres(html):
+    soup = HI.soupinit(html=html)
+    content = soup.body.find("div", attrs={"class": "body pod_gameinfo_left"})
+    if not content:
+        content = soup.body.find("div", attrs={"class": "pod pod_gameinfo"})
+    if not content:
+        print("no idea where right table is", html)
+    content = content.ul
+    lii = content.find_all("li")
+    for li in lii:
+        b = HI.undiv(li.find('b'))
+        if b[:3] == "Gen":  # "Genre:"
+            genri = li
+            break
+
+    if "genri" not in locals():
+        errors.write("Unknown Genres:" + html)
+        return "Unknown"
+
+    genri = genri.find_all("a", href=True)
+    genri = [HI.undiv(genre) for genre in genri]
+    return genri
+
+
+def extract_table(page_num, cutoff_wankers=0, genre_ignore=()):
     global rank
     page = genpage + str(page_num)
     print('\npage:', page, rank)
@@ -70,10 +93,14 @@ def ExtractTable(page_num, cutoff_wankers=0):
         name = name.replace(',', '.').replace('ū', 'u').replace('é', 'e').replace('ä', 'a')
         # '\u016b'='ū' '\u00e9'='é' '\xe4'='ä. é appears particularly in all them pokémon games.
         game_html = HI.gethtml(game)
-        wankers = ExtractWankers(game_html, name)
+        wankers = extract_wankers(game_html, name)
         if int(wankers) < cutoff_wankers:
             continue
-        date = ExtractDate(game_html)
+        genri = extract_genres(game_html)
+        for genre in genre_ignore:
+            if genre in genri:
+                continue
+        date = extract_date(game_html)
         line = ",".join([name, system, rank, wankers, date])
         line = line.replace('&amp;', '&')
         report.write(line + '\n')
@@ -82,7 +109,7 @@ def ExtractTable(page_num, cutoff_wankers=0):
 
 if __name__ == '__main__':
     home = "https://gamefaqs.gamespot.com"
-    report = open('GameFAQs3.csv', 'w+')
+    report = open('GameFAQs4.csv', 'w+')
     errors = open("errors.log", 'w+')
     genpage = home + "/games/rankings?min_votes=2&dlc=1" + '&page='
     cutoff_rank = 3.7
@@ -90,6 +117,6 @@ if __name__ == '__main__':
     page_num = 0
     report.write('Name,System,rating,rankers,date\n')
     while rank >= cutoff_rank:
-        ExtractTable(page_num, cutoff_wankers=15)
+        extract_table(page_num, cutoff_wankers=0, genre_ignore=("sports",))
         page_num += 1
         rank = float(rank)
