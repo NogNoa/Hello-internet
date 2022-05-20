d09132a61457cadf79a7dadb9d0f1ea9335a482d
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-05-20 16:54:41 +0300

    listing all new modules from all versions

--- pylibhist.py
+++ pylibhist.py
@@ -1,20 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://docs.python.org/release/1.4/lib/"
+urli = ["https://docs.python.org/release/1.4/lib/",
+        "https://docs.python.org/release/1.5/lib/",
+        "https://docs.python.org/release/1.5.1/lib/",
+        "https://docs.python.org/release/1.5.1p1/lib/",
+        "https://docs.python.org/release/1.5.2/lib/",
+        "https://docs.python.org/release/1.5.2p1/lib/",
+        "https://docs.python.org/release/1.5.2p2/lib/",
+        "https://docs.python.org/release/1.6/lib/",
+        "https://docs.python.org/release/2.0/lib/",
+        ]
 
-version = url.split("/")[4]
 codex_nom = "python library history.txt"
+with open(codex_nom, "w+") as file:
+    pass
 
-with open(codex_nom, "a+") as file:
-	file.write(version+":\n")
+seen_modules = set()
 
-rsp = requests.get(url, verify=True, allow_redirects=True)
-soup = BeautifulSoup(rsp.text, features="html.parser")
-body = soup.body
-moduli = body.find_all("code")
-with open(codex_nom, "a+") as file:
-	for module in moduli:
-		file.write(f"\t{module.text}\n")
-		print(module.text)
-	file.write(2*"\n")
+for url in urli:
+    version = url.split("/")[4]
+    with open(codex_nom, "a") as file:
+        file.write(version + ":\n")
+
+    rsp = requests.get(url, verify=True, allow_redirects=True)
+    soup = BeautifulSoup(rsp.text, features="html.parser")
+    body = soup.body
+    moduli = body.find_all(["code", "tt"])
+    with open(codex_nom, 'a') as file:
+        for module in moduli:
+            module = module.text
+            if module not in seen_modules:
+                file.write(f"\t{module}\n")
+                print(module)
+                seen_modules.add(module)
+        file.write("\n")
