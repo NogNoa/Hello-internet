12867acf5af0d575e66bfdad40dd658759311312
NogNoa <53447424+NogNoa@users.noreply.github.com>
2023-02-21 17:00:15 +0200

    one path argument

--- amoeba.py
+++ amoeba.py
@@ -4,27 +4,27 @@ import requests
 
 from TableRead import *
 
-root_adress = "https://web.archive.org/web/20060106073452/http://www.cs.vu.nl/pub/amoeba/amoeba5.3/"
+root_adress = "https://web.archive.org/web/20060106073452/http://www.cs.vu.nl/pub/amoeba/"
 
 os.chdir(r"G:\Source\sites\Amoeba")
 
 
-def save_as(name, inter, file_type=""):
-    resp = requests.get(root_adress + inter + name)
+def save_as(local_path, file_type=""):
+    resp = requests.get(root_adress + local_path)
     if not file_type:
-        with open(inter + name, "w+", encoding=resp.apparent_encoding) as codex:
+        with open(local_path, "w+", encoding=resp.apparent_encoding) as codex:
             codex.write(resp.text)
     else:
-        with open(inter + name, "wb+") as codex:
+        with open(local_path, "wb+") as codex:
             codex.write(bytes(resp.text))
         if file_type != "archive":
             print(file_type)
 
 
-def extract_directory(base_dir, inter=""):
-    inter += base_dir
-    url = root_adress + inter
+def extract_directory(local_path):
+    url = root_adress + local_path
     soup = soup_init(url=url)
+    os.mkdir(local_path)
     for tag in soup.body:
         tag.extract()
         if tag.string == " END WAYBACK TOOLBAR INSERT ":
@@ -35,8 +35,12 @@ def extract_directory(base_dir, inter=""):
             print(f'url="{url}", element="{img.next}"')
             continue
         if img["src"].endswith("unknown.gif"):
-            save_as(link, inter)
+            save_as(local_path + link)
         elif img["src"].endswith("folder.gif"):
-            extract_directory(link, inter)
+            extract_directory(local_path + link)
         elif img["src"].endswith("compressed.gif"):
-            save_as(link, inter, "archive")
+            save_as(local_path + link, "archive")
+
+
+if __name__ == "__main__":
+    extract_directory("amoeba5.3")
