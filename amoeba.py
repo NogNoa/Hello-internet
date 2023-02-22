d4212357493ef700c5b780078a71c44d3c4976eb
NogNoa <53447424+NogNoa@users.noreply.github.com>
2023-02-22 21:32:43 +0200

    extract site

--- amoeba.py
+++ amoeba.py
@@ -3,10 +3,12 @@ import os
 import requests
 
 from TableRead import *
+from bs4 import BeautifulSoup
 
 root_adress = "https://"
 
-def save_as(local_path):
+
+def save_as(local_path: str):
     if os.path.exists(local_path) and os.path.getsize(local_path):
         return
     sleep(random.random() * 10)
@@ -15,7 +17,7 @@ def save_as(local_path):
         codex.write(resp.content)
 
 
-def wayback_strip(soup):
+def wayback_strip(soup: BeautifulSoup) -> BeautifulSoup:
     for tag in soup.head:
         tag.extract()
         if tag.string == " End Wayback Rewrite JS Include ":
@@ -31,7 +33,7 @@ def wayback_strip(soup):
     return soup
 
 
-def extract_directory(local_path, wayback=False):
+def extract_directory(local_path: str, wayback=False):
     url = root_adress + local_path
     soup = soup_init(url=url)
     try:
@@ -55,15 +57,30 @@ def extract_directory(local_path, wayback=False):
         else:
             print(suffix)
 
-def extract_site(local_path, wayback=False):
+
+def extract_site(local_path: str, wayback=False):
     url = root_adress + local_path
-    soup = soup_init(url=url)
+    soup: BeautifulSoup = soup_init(url=url)
     try:
         os.mkdir(local_path)
     except FileExistsError:
         pass
     if wayback:
         soup = wayback_strip(soup)
+    with open(local_path, "w+") as codex:
+        codex.write(soup.text)
+    for tag in soup.head:
+        if tag.has_attr("href"):
+            link = tag["href"]
+            if link.startswith("http") and not link.startswith(root_adress):
+                save_as(local_path + link)
+            elif link.startswith("#"):
+                continue
+            else:
+                extract_site(local_path + link)
+        if tag.has_attr("src"):
+            save_as(local_path + tag["src"])
+
 
 if __name__ == "__main__":
     root_adress = "https://web.archive.org/web/20060106073452/http://www.cs.vu.nl/pub/amoeba/"
