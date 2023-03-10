34489db3507bb0979430d1062ac1a74d33ec36f1
NogNoa <53447424+NogNoa@users.noreply.github.com>
2023-03-10 17:15:27 +0200

    pylibhist2 works

--- pylibhist2.py
+++ pylibhist2.py
@@ -11,7 +11,10 @@ def get_module_names(url):
     rsp = requests.get(url, verify=True, allow_redirects=True)
     soup = BeautifulSoup(rsp.text, features="html.parser")
 
-    modules = soup.find_all("code", attrs={"class":"module"})
+    modules = soup.find_all("code")
+    modules = set(modules).union(soup.find_all("tt"))
+    modules = filter(lambda m:not m.has_attr("class") or m["class"][0] == "module",modules)
+
 
     return [module.text for module in modules]
 
@@ -32,7 +35,7 @@ def main():
             "https://docs.python.org/release/1.6/lib/",
             "https://docs.python.org/release/2.0/lib/",
     ]
-    modules = reduce(urls, (lambda back, url: back.add(*get_module_names(url))), set())
+    modules = reduce((lambda back, url: back.union(set(get_module_names(url)))), urls, set())
     with open("python library history2.txt", "w+") as codex:
         write_modules(codex, modules)
 
